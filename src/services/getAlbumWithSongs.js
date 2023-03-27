3e99583499bf1bc2bedf9221ed83a329268f8c5a
maikelreyy <104846653+maikelreyy@users.noreply.github.com>
2023-03-27 16:22:16 +0200

    Update getAlbumWithSongs.js

--- src/services/getAlbumWithSongs.js
+++ src/services/getAlbumWithSongs.js
@@ -1,10 +1,9 @@
 //necesitamos mandarle el id de alguna forma,asique se lo mandamos por parametro.
 
 const getAlbumWithSongs = async (id) => {
+  /*hay que indicarle que el elemento que queremos son las canciones, por eso lo de entity=song*/
   const res = await fetch(
     `https://api.allorigins.win/get?url=${encodeURIComponent(
-      //camiamos la api a la que le vamos a hacer la peticion, porque?
-      //hay que indicarle que el elemento que queremos son las canciones, por eso lo de entity=song
       `https://itunes.apple.com/lookup?id=${id}&entity=song`
     )}`
   );
@@ -15,15 +14,6 @@ const getAlbumWithSongs = async (id) => {
 
   const body = await res.json();
 
-  //despues de hacer esto, hacemos un console.log(body) para ver en consola los datos que nos devuelve la api y poder crear la peticion correctamente.
-  //esto nos devuelve un objeto llamado contents, entonces:
-  //hacemos un console.log(JSON.parse(body.contents)) => console.log(JSON.parse(body.contents).results) para ver los datos del objeto parseado. (resultCount) y array de results( cada objeto es una cancion)
-
-  //destructuring del array que nos da la api de results
-  //con el [info, ...songs] estamos diciendo que la primera posicion (info del album) la guarde en info y todo lo demas en songs (las canciones)
-  //con los ... decimos que meta todo lo que hay despues de lo ultimo que indicamos.
-  //const [info, ...songs] = JSON.parse(body.contents).results //non sei porque results vai fora do parentesis
-  //filtramos os datos que recollemos da api facendo destructuring
   const album = (obj) => {
     const id = obj.collectionId;
     const albumName = obj.collectionCensoredName;
