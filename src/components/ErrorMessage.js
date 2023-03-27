1000036c40faa3962398867a716c5844b5b5bd65
maikelreyy <104846653+maikelreyy@users.noreply.github.com>
2023-03-27 16:12:20 +0200

    Update ErrorMessage.js

--- src/components/ErrorMessage.js
+++ src/components/ErrorMessage.js
@@ -1,5 +1,3 @@
-//porque hay que poñer o parentesis e as chaves? porque tamos traendo o error do hook useTopAlbums?
-
 const errorMessage = ({ error }) => {
   return <p className="errorMessage">{error}</p>;
 };
